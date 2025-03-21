a9b33e21966f3161b2042a53cd30e899b590464e
Lean Albuquerque <lean.albuquerque@mail.uft.edu.br>
2025-03-20 21:40:14 -0300

    fixed remove func

--- heaps/main.go
+++ heaps/main.go
@@ -21,31 +21,30 @@ func (h *heap) heapifyUp() {
 			break
 		}
 	}
-}	
+}
 
 func (h *heap) insert(data int) {
 	h.data = append(h.data, data)
 	h.heapifyUp()
 }
 
-func (h *heap) heapifyDown () {
-	index := len(h.data)-1
-	lenght := len(h.data)
-	for{
+func (h *heap) heapifyDown(index int) {  // Changed to take index as parameter
+	length := len(h.data)               // Fixed typo 'lenght' to 'length'
+	for {
 		largest := index
 		left := 2*index + 1
 		right := 2*index + 2
 
-		if left < lenght && h.data[left] > h.data[largest]{
+		if left < length && h.data[left] > h.data[largest] {
 			largest = left
 		}
-		if right < lenght && h.data[right] > h.data[largest]{
+		if right < length && h.data[right] > h.data[largest] {
 			largest = right
 		}
-		if largest != index{
+		if largest != index {
 			h.data[index], h.data[largest] = h.data[largest], h.data[index]
 			index = largest
-		}else{
+		} else {
 			break
 		}
 	}
@@ -56,14 +55,14 @@ func (h *heap) remove() int {
 		return -1
 	}
 	max := h.data[0]
-	last :- len(h.data)-1
+	last := len(h.data) - 1           // Fixed syntax error in variable declaration
 	h.data[0] = h.data[last]
 	h.data = h.data[:last]
 
-	if len(data.h) > 0{
-		h.heapifyDown(0)
+	if len(h.data) > 0 {              // Fixed typo 'data.h' to 'h.data'
+		h.heapifyDown(0)              // Pass 0 as starting index
 	}
-	return 0
+	return max                        // Return the removed max value instead of 0
 }
 
 func main() {
@@ -73,9 +72,7 @@ func main() {
 	myHeap.insert(15)
 	myHeap.insert(20)
 
-	// Output: [15 5 10]
-	for _, v := range myHeap.data {
-		fmt.Println(v)
-	}
-
+	// Should output: [20 15 10 5]
+	// This is a max heap where parent is always greater than children
+	fmt.Println(myHeap.data)
 }
